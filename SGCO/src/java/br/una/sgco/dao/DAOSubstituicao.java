255770a89e58e6a225ee6b4b89b14611bc783ebf
shamanti <shamanti@7ffba141-daf8-90fd-fd41-a48ba7ae71d5>
2012-03-12 21:17:12 +0000

    12/03/2012
    CORREÇÕES
    
    - Cadastro de Estádio
     > Removido Campo cam_codigo das implementacoes de listagem e insersão
    - Substituição
     > Inserido campo tim_codigo no banco de dados
     ALTER TABLE `sgc_substituicao_sub` ADD `tim_codigo` INT NOT NULL AFTER `JGS_CODIGO` 
    
    - Cartões
     > Inserido campo tim_codigo no banco de dados
     ALTER TABLE `sgc_cartoes_crt` ADD `tim_codigo` INT NOT NULL 
    
    - Placar
     > Inserido campo tim_codigo no banco de dados
     ALTER TABLE `sgc_placar_plc` ADD `tim_codigo` INT NOT NULL  
    
    
    - Arbitragem
     > Removido cam_codigo
    
    - Jogo
     > Alterada nome da coluna do juiz reserva
    
    
    ERROS:
    
    - Edição de Estádio
     > Combobox não vem selecionada no estado cadastrado.
    - Jogo
     > Erro no tratamento de data/hora para gravação e Alteração
    
    
    
    Parte publica:
    
    > Exibir campeonatos
    > Home
    > Erros de SQL, por causa das datas (Diferentes no SQL Server)

--- SGCO/src/java/br/una/sgco/dao/DAOSubstituicao.java
+++ SGCO/src/java/br/una/sgco/dao/DAOSubstituicao.java
@@ -22,7 +22,7 @@ public class DAOSubstituicao {
     public static void inserir (TOSubstituicao subs, Connection c) throws Exception {
 
         String sql = " insert into sgc_substituicao_sub (jgs_codigo, tim_codigo, "
-                    + " jog_codigo_entrou, jog_codigo_saiu)"
+                + "                                     jog_codigo_entrou, jog_codigo_saiu)"
                     + " values (?, ?, ?, ?)";
 
         Data.executeUpdate(c, sql, new Object[] {subs.getJogo().getCodigo(),
@@ -34,7 +34,7 @@ public class DAOSubstituicao {
     public static void excluir (TOSubstituicao subs, Connection c) throws Exception {
 
         String sql = " delete from sgc_substituicao_sub where jgs_codigo = ? "
-//                    + " and tim_codigo = ? "
+                    + " and tim_codigo = ? "
                     + " and jog_codigo_entrou = ? "
                     + " and jog_codigo_saiu = ? ";
 
@@ -45,19 +45,19 @@ public class DAOSubstituicao {
     }
 
     public static JSONArray obterTodos(TOCampeonato campeonato, Connection c) throws Exception {
-        String sql = " select sub.jog_codigo_entrou, sub.jog_codigo_saiu, sub.jgs_codigo,  " //sub.tim_codigo,
-                    + " jog1.jog_nome as jog_nome_entrou, jog2.jog_nome as jog_nome_saiu "//,
-//                    + " tim1.tim_nome as time_mandante, tim2.tim_nome as time_visitante "
+        String sql = " select sub.jog_codigo_entrou, sub.jog_codigo_saiu, sub.jgs_codigo, sub.tim_codigo, "
+                    + " jog1.jog_nome as jog_nome_entrou, jog2.jog_nome as jog_nome_saiu, "
+                    + " tim1.tim_nome as time_mandante, tim2.tim_nome as time_visitante "
                     + " from sgc_substituicao_sub sub "
                     + "   inner join sgc_jogos_jgs jgs on sub.jgs_codigo = jgs.jgs_codigo "
                     + "   inner join sgc_jogador_jog jog1 "
                     + "     on jog1.jog_codigo = sub.jog_codigo_entrou "
                     + "   inner join sgc_jogador_jog jog2 "
                     + "     on jog2.jog_codigo = sub.jog_codigo_saiu "
-//                    + "   inner join sgc_time_tim tim1 "
-//                    + "     on tim1.tim_codigo = jgs.tim_codigo_mandante "
-//                    + "   inner join sgc_time_tim tim2 "
-//                    + "     on tim2.tim_codigo = jgs.tim_codigo_visitante "
+                    + "   inner join sgc_time_tim tim1 "
+                    + "     on tim1.tim_codigo = jgs.tim_codigo_mandante "
+                    + "   inner join sgc_time_tim tim2 "
+                    + "     on tim2.tim_codigo = jgs.tim_codigo_visitante "
                     + " where jgs.cam_codigo = ?";
 
         JSONArray ja = new JSONArray();
@@ -67,13 +67,13 @@ public class DAOSubstituicao {
         while (rs.next()) {
             JSONObject jo = new JSONObject();
             jo.put("jogoCodigo", rs.getInt("jgs_codigo"));
-            //jo.put("timeCodigo", rs.getInt("tim_codigo"));
+            jo.put("timeCodigo", rs.getInt("tim_codigo"));
             jo.put("jogadorCodigoEntrou", rs.getInt("jog_codigo_entrou"));
             jo.put("jogadorCodigoSaiu", rs.getInt("jog_codigo_saiu"));
             jo.put("jogadorNomeEntrou", rs.getString("jog_nome_entrou"));
             jo.put("jogadorNomeSaiu", rs.getString("jog_nome_saiu"));
-            //jo.put("timeMandante", rs.getString("time_mandante"));
-            //jo.put("timeVisitante", rs.getString("time_visitante"));
+            jo.put("timeMandante", rs.getString("time_mandante"));
+            jo.put("timeVisitante", rs.getString("time_visitante"));
             ja.put(jo);
         }
 
